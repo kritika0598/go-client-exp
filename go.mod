74e09eaf8992e6d7264033bae9e7ca1e73259b95
kritika.singhal <3319-kritika.singhal@users.noreply.source.golabs.io>
2021-10-08 08:27:41 +0530

    feat: Containerize binary

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module projectone
+module lister
 
 go 1.13
 
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"k8s.io/client-go/rest"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -14,28 +15,32 @@ import (
 func main() {
 	kubeconfig := flag.String("kubeconfig", "/Users/kritika/.kube/config", "location to your kube config file")
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	//fmt.Println(config)
 	if err != nil {
-		//handle error
+		fmt.Printf("Error %s building from flags\n\n", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("Error %s  Getting InClusterConfig\n\n", err.Error())
+		}
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		//handle error
+		fmt.Printf("Error %s, creating client set \n\n", err.Error())
 	}
-	pods, err := clientset.CoreV1().Pods("sms-test").List(context.Background(), metav1.ListOptions{})
+	pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		//handle error
+		fmt.Printf("Error %s, While listenting all the pods from default namespace\n\n", err.Error())
 	}
 	fmt.Println("PODS")
-
 	for _, pod := range pods.Items {
 		fmt.Println("%s", pod.Name)
 	}
 
-	deployments, err := clientset.AppsV1().Deployments("sms-test").List(context.Background(), metav1.ListOptions{})
+	deployments, err := clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error %s, While listenting all the deployments from default namespace\n\n", err.Error())
+	}
 	fmt.Println("DEPLOYMENTS")
 	for _, deployment := range deployments.Items {
 		fmt.Println("%s", deployment.Name)
 	}
-
 }
